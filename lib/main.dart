d32a6f608f91dfa4dd182736a3ff351656a27f40
DavidKwon7 <khjunejune10@gmail.com>
2022-06-25 18:53:36 +0900

    http draft

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HttpApp(),
+    );
+  }
+}
+
+class HttpApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HttpApp();
+
+}
+
+class _HttpApp extends State<HttpApp> {
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Http Example'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('$result'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var url = 'https://api.github.com/users/DavidKwon7/repos';
+          var response = await http.get(Uri.parse(url));
+          setState(() {
+            result = response.body;
+          });
+        },
+        child: Icon(Icons.file_download),
+      ),
+    );
+  }
+
+}
